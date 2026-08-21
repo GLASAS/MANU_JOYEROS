0e5ff7ccd92251576ff96f01386054eb3b14626c
GLASAS <138023380+GLASAS@users.noreply.github.com>
2026-08-21 12:39:41 -0500

    js/config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+/**
+ * MANU JOYEROS - Configuración Global
+ */
+const CONFIG = {
+  // Reemplaza esta URL con la URL de tu implementación de Google Apps Script (Web App)
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyYQ_djgJquooysR9DDwa7p4MAiHcCR0IokHqv9XxtwnDMkxbi8kX7A1R58H8tTOdRy/exec",
+  NOMBRE_EMPRESA: "MANU JOYEROS"
+};
