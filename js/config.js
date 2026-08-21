8d35c3ca7181097c239f0e93058d03f664ed0c9d
GLASAS <138023380+GLASAS@users.noreply.github.com>
2026-08-21 21:36:14 +0000

    Update version number in config.js

--- js/config.js
+++ js/config.js
@@ -4,5 +4,5 @@
 const CONFIG = {
   APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyj5fBm9XhZRz21RxfN488eEXpFRxwZcDxRlHb2sXUyjDU6F-IfnCeMU9nSyISlT74/exec",
   NOMBRE_EMPRESA: "MANU JOYEROS",
-  VERSION: "V1.1625"
+  VERSION: "V1.1630"
 };
