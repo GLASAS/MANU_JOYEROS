8f3aa093b4b930783b2e4b9ffad7b21fe0184dbc
GLASAS <138023380+GLASAS@users.noreply.github.com>
2026-08-21 14:57:22 -0500

    Change APPS_SCRIPT_URL in config.js
    Updated the APPS_SCRIPT_URL to a new endpoint.

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@
  */
 const CONFIG = {
   // Reemplaza esta URL con la URL de tu implementación de Google Apps Script (Web App)
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw4qrA_oPtR646AR_RcRTtVFejt77VYWicIWnXcdkORsuoColtZ3AiTCLhCy141dYWq/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyQ4Ydvb2m2Jnef3VVNlziVOulDRvbHoFE399rFKZbKaur_K5itOc2sJU-1Itl6s5KM/exec",
   NOMBRE_EMPRESA: "MANU JOYEROS"
 };
