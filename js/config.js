0091f3d01bc072af8792bfc61c6dd93f29b5ab3c
GLASAS <138023380+GLASAS@users.noreply.github.com>
2026-08-21 13:14:24 -0500

    config.js

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@
  */
 const CONFIG = {
   // Reemplaza esta URL con la URL de tu implementación de Google Apps Script (Web App)
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw4wjSPFadg-S1zFG0hsk2pj-DNZM0r0Zedxrs_EEl-LYyRnLp_s0SGDc7RESOMxTHP/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw4qrA_oPtR646AR_RcRTtVFejt77VYWicIWnXcdkORsuoColtZ3AiTCLhCy141dYWq/exec",
   NOMBRE_EMPRESA: "MANU JOYEROS"
 };
