c85aff897c01933ce9d23f1a733e07c4eab0a298
GLASAS <138023380+GLASAS@users.noreply.github.com>
2026-08-21 12:50:45 -0500

    1
    Updated the APPS_SCRIPT_URL to the new implementation.

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@
  */
 const CONFIG = {
   // Reemplaza esta URL con la URL de tu implementación de Google Apps Script (Web App)
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyYQ_djgJquooysR9DDwa7p4MAiHcCR0IokHqv9XxtwnDMkxbi8kX7A1R58H8tTOdRy/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbx9CmKDs6lzH-BU1UKZ_BGzsxlfRWXwK-em-H5ol-9_4UvCRQFxjS3ZGC7YuXIXLSI1/exec",
   NOMBRE_EMPRESA: "MANU JOYEROS"
 };
