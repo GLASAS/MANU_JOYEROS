8a86441b063dba6ddabf557dfb41aae5000a951b
GLASAS <138023380+GLASAS@users.noreply.github.com>
2026-08-24 06:51:17 -0500

    Update APPS_SCRIPT_URL and VERSION in config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
  * MANU JOYEROS - Configuración Global y Versión del Sistema
  */
 const CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw1Xlf6pYBZepqKO0QEmCC7B8VT9DraJcEcVp-gYdrH1Bs7IpdVoI3VzgZvV3NV_hHl/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxxFVC_IW8_GhlXaUCXDZeWcexOT9ynLCorSSzK1heiAKapj-Sl3vobUCTPmBJEioy_/exec",
   NOMBRE_EMPRESA: "MANU JOYEROS",
-  VERSION: "V1.641"
+  VERSION: "V1.649"
 };
