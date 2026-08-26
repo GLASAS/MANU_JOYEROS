263c782e4fd670da75361cef3158f42198c4d5c0
GLASAS <138023380+GLASAS@users.noreply.github.com>
2026-08-26 11:35:23 -0500

    Update phone number and version in config

--- js/config.js
+++ js/config.js
@@ -5,9 +5,9 @@ const CONFIG = {
   APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbybbv5IVJy_1tI1tagtHC6e-ZuAlfWeaLgYqz1R_h-YXFWSVWK2qE2SpvGGOJfLiVvD/exec",
   NOMBRE_EMPRESA: "MANU JOYEROS",
   NIT: "900.000.000-1",
-  TELEFONO: "+57 (300) 8123125",
+  TELEFONO: "+57 (320) 8123125",
   DIRECCION: "Ak 7 #115-60 Local 217",
   EDIFICIO_O_LOCAL: "Hacienda Santa Barbara",
   CIUDAD: "Bogotá D.C., Colombia",
-  VERSION: "V1.1100"
+  VERSION: "V1.1133"
 };
