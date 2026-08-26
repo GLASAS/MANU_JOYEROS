22fc43c6526cb952c50a38ea570303640ec16f76
GLASAS <138023380+GLASAS@users.noreply.github.com>
2026-08-26 11:36:57 -0500

    Update address in config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   NOMBRE_EMPRESA: "MANU JOYEROS",
   NIT: "900.000.000-1",
   TELEFONO: "+57 (320) 8123125",
-  DIRECCION: "Ak 7 #115-60 Local 217",
+  DIRECCION: "Ak 7 #115-60 Local 117",
   EDIFICIO_O_LOCAL: "Hacienda Santa Barbara",
   CIUDAD: "Bogotá D.C., Colombia",
   VERSION: "V1.1133"
