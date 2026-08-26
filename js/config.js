7bd6540311d4b6e6fe626b33902b667484052112
GLASAS <138023380+GLASAS@users.noreply.github.com>
2026-08-26 18:30:31 +0000

    Update configuration details in config.js
    Updated APP_SCRIPT_URL, address, and version number.

--- js/config.js
+++ js/config.js
@@ -2,12 +2,12 @@
  * MANU JOYEROS - Configuración Global y Versión del Sistema
  */
 const CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbybbv5IVJy_1tI1tagtHC6e-ZuAlfWeaLgYqz1R_h-YXFWSVWK2qE2SpvGGOJfLiVvD/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzDYEBkjV4kbYGrgNm-iLCapZbplTievOm3iOWY_FGPeGrTnAueFM6E_SbxBnYlyCus/exec",
   NOMBRE_EMPRESA: "MANU JOYEROS",
   NIT: "900.000.000-1",
   TELEFONO: "+57 (320) 8123125",
-  DIRECCION: "Ak 7 #115-60 Local 1320",
+  DIRECCION: "Ak 7 #115-60 Local 1330",
   EDIFICIO_O_LOCAL: "Hacienda Santa Barbara",
   CIUDAD: "Bogotá D.C., Colombia",
-  VERSION: "V1.1320"
+  VERSION: "V1.1330"
 };
