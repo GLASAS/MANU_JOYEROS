85b72ce5d4f50527af1d91d698105017bb623b63
GLASAS <138023380+GLASAS@users.noreply.github.com>
2026-08-26 19:57:44 +0000

    Update company details in config.js

--- js/config.js
+++ js/config.js
@@ -4,9 +4,9 @@
 const CONFIG = {
   APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw2dhdfkcK5j1ISFFb0vhMBuxz30CO4tECVTTeykg4be72WPeQoj_Oj6JerBfcQ9_C9/exec",
   NOMBRE_EMPRESA: "MANU JOYEROS",
-  NIT: "900.000.000-1",
-  TELEFONO: "+57 (320) 8123125",
-  DIRECCION: "Ak 7 #115-60 Local 1415",
+  NIT: "902.078.370-8",
+  TELEFONO: "+57 (311) 888 6137",
+  DIRECCION: "Calle 114 # 6 A - 92, Local 301 ",
   EDIFICIO_O_LOCAL: "Hacienda Santa Barbara",
   CIUDAD: "Bogotá D.C., Colombia",
   VERSION: "V1.1415"
