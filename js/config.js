8e53449a9c2852bf8b2c887137362923fdcfc029
GLASAS <138023380+GLASAS@users.noreply.github.com>
2026-08-26 19:20:01 +0000

    Update configuration values for app settings

--- js/config.js
+++ js/config.js
@@ -2,12 +2,12 @@
  * MANU JOYEROS - Configuración Global y Versión del Sistema
  */
 const CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwuTHb6PXB-8x4w3fELs_ehJfg6cxRj3Pg0eE5N23QjdqYiDN89M-HFRGaj98NwaMvE/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw2dhdfkcK5j1ISFFb0vhMBuxz30CO4tECVTTeykg4be72WPeQoj_Oj6JerBfcQ9_C9/exec",
   NOMBRE_EMPRESA: "MANU JOYEROS",
   NIT: "900.000.000-1",
   TELEFONO: "+57 (320) 8123125",
-  DIRECCION: "Ak 7 #115-60 Local 1410",
+  DIRECCION: "Ak 7 #115-60 Local 1415",
   EDIFICIO_O_LOCAL: "Hacienda Santa Barbara",
   CIUDAD: "Bogotá D.C., Colombia",
-  VERSION: "V1.1410"
+  VERSION: "V1.1415"
 };
